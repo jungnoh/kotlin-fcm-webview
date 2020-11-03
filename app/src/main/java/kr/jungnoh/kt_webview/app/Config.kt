6def81f97cf44c6c4657d8820d72cb270383f195
Junghoon Noh <jungnoh.dev@gmail.com>
2020-11-03 22:39:12 +0900

    Update host to sample site

--- app/src/main/java/kr/jungnoh/kt_webview/app/Config.kt
+++ app/src/main/java/kr/jungnoh/kt_webview/app/Config.kt
@@ -2,8 +2,8 @@ package kr.jungnoh.kt_webview.app
 
 class Config {
     companion object {
-        const val host = "https://jungnoh.me"
+        const val host = "https://jungnoh-fcm-example.herokuapp.com"
         const val userAgent = "jungnoh-wv"
-        const val sessionKey = "connect.sid"
+        const val sessionKey = "session"
     }
 }
